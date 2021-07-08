2b445e68cea7eae7696e63effb357e38fb54deb9
Allen <jihoshin.28@gmail.com>
2021-07-07 22:44:58 -0700

    sidebar has searchbar and add chat button

--- src/windows/Sidebar.js
+++ src/windows/Sidebar.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from '../containers/Navbar'
 
 const Sidebar = () => {
     return(
         <div class = "sidebar">
-            <h1> Sidebar</h1>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <div class="sidebar-content">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button class="btn btn-primary" type="submit">
+                            +
+                        </button>
+                    </div>
+                </div>
+                
+            </nav>
         </div>
     )
 }
