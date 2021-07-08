ac9dab08115883dcc3e6eb7d553903b364c4de54
Allen <jihoshin.28@gmail.com>
2021-07-07 20:37:39 -0700

    create chat form

--- src/pages/CreateChat.js
+++ src/pages/CreateChat.js
@@ -9,8 +9,35 @@ const CreateChat = () => {
         setRoomId(id)
     }, [])
     return (
-        <div>
-            <h1>Create a Chat Room</h1>
+        <div class = "container">
+            <div class = "row">
+                <h1>Create a Chat Room</h1>
+            </div>
+            <div class = "create-chat-div border border-2">
+                <div class = "row">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="inputGroup-sizing-default"></span>
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Type of Chat</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Add Friends</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+
             
         </div>
     );
