7b5366062be02b81dba3b9121c47a0403d3fe739
Allen <jihoshin.28@gmail.com>
2021-08-24 16:24:45 -0700

    login functions

--- src/windows/Login.js
+++ src/windows/Login.js
@@ -2,17 +2,21 @@ import {useState} from 'react';
 
 const Login = () => {
 
+    const onChange = (e) => {
+        console.log(e.target)    
+    }
+
     return (
         <div className = "login-page">
             <div className = "container border rounded login-box">
                 <h2 className = "login-header">Login</h2>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="inputGroup-sizing-default">Username</span>
-                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                    <input onChange = {(e) => onChange(e)} name = "username" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
                 </div>  
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="inputGroup-sizing-default">Password</span>
-                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                    <input onChange = {(e) => onChange(e)} name = "password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
                 </div>
                 <button className = "btn btn-primary">Login</button>
             </div>
