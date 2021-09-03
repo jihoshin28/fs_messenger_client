7451998803f56fec450e366e0e650c3ed9930486
Allen <jihoshin.28@gmail.com>
2021-09-03 15:38:18 -0700

    added dropdown for search bar

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import SidebarChat from '../components/SidebarChat'
 
 const Sidebar = ({chats, chat_id, current_user, setChat}) => {
+    let [friendSearch, setFriendSearch] = useState("")
+    let ref = useRef()
+
 
     let onFocus = (chat_id) => {
         setChat(chat_id)
@@ -11,7 +14,6 @@ const Sidebar = ({chats, chat_id, current_user, setChat}) => {
     let renderSidebarChats = () => {
         if(!!chats){
             if(chats.length > 0 && !!current_user){
-    
                 return chats.map((chat, index) => {
                     let chat_users = chat.users.filter((user) => user._id !== current_user._id)
                     if(chat._id === chat_id){
@@ -26,12 +28,36 @@ const Sidebar = ({chats, chat_id, current_user, setChat}) => {
         }
     }
 
+    let showDropdown = () => {
+        if(friendSearch === ""){
+            return ""
+        } else {
+            return "show"
+        }
+    }
+
+    let renderFriends = (input) => {
+        
+        return(
+            <React.Fragment>
+                <li><button class="dropdown-item" type="button">Action</button></li>
+                <li><button class="dropdown-item" type="button">Another action</button></li>
+                <li><button class="dropdown-item" type="button">Something else here</button></li>
+            </React.Fragment>
+        )
+    }
+
     return(
         <div class = "sidebar">
             <nav class="sticky-nav navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <div class="sidebar-nav">
-                        <input class="form-control me-2" type="search" placeholder="Search People" aria-label="Search"/>
+                        <div class="dropdown">
+                            <input onChange = {(e) => setFriendSearch(e.target.value)} class="form-control me-2" type="search" placeholder="Search Friends" aria-label="Search"/>
+                            <ul class= {`dropdown-menu ${showDropdown()}`} aria-labelledby="dropdownMenu2">
+                                {renderFriends(friendSearch)}
+                            </ul>
+                        </div>
                         <Link class="btn btn-primary" to = "/create_chat">
                             +
                         </Link>
